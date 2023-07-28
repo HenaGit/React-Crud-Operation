3d95f9f4aa42699d35e92bd27c2ce1c046db1958
HenokG <heni.gebrehiwot@gmail.com>
2023-07-28 11:57:30 -0600

    Add Look and Feel ContactOPedia

--- src/Components/ContactPages/AddContact.jsx
+++ src/Components/ContactPages/AddContact.jsx
@@ -1,6 +1,7 @@
 const AddContact = () => {
     return (
-        <div className="border row text-white p-2">
+      <div className="border col-12 text-white p-2">
+      <div className="row p-2">
         <div className="col-12 text-white-50">Add a new Contact</div>
         <div className="col-12 col-md-4 p-1">
           <input
@@ -21,7 +22,10 @@ const AddContact = () => {
           ></input>
         </div>
         <div className="col-12 col-md-6 offset-md-3 p-1">
-          <button className="btn btn-primary btn-sm form-control">Create</button>
+          <button className="btn btn-primary btn-sm form-control">
+            Create
+          </button>
+        </div>
         </div>
       </div>
     );
